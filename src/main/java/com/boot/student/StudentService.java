d8f9c40f985e086c6c44fa616ef15e10d7b0a56c
kasim zahid <106993571+kzhid@users.noreply.github.com>
2024-04-22 18:33:56 +0100

    updating configuration with postgres
    made it actually work a little more with postgres lol

--- src/main/java/com/boot/student/StudentService.java
+++ src/main/java/com/boot/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.boot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -11,17 +12,15 @@ import java.util.List;
 @Service
 public class StudentService {
 
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Mariam",
-                        "Mariam.jamal@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
+    private final StudentRepository studentRepository;
 
-                )
-        );
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+             return studentRepository.findAll();
     }
 
 
